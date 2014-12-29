451ce317f32ffeebd93a0c83983067cd214f7eae
Daniel Duque <dduque@upsidecorp.ch>
2014-12-29 16:40:47 -0430

    ajustes en confirmar para Flashsales

--- protected/views/bolsa/confirm.php
+++ protected/views/bolsa/confirm.php
@@ -315,11 +315,15 @@
 							$inventario = Inventario::model()->findByPk($uno->inventario_id);	
 
 							if($inventario->estado ==1){
-
 								$principal = Imagenes::model()->findByAttributes(array('orden'=>1,'producto_id'=>$inventario->producto_id));
-
 								$producto = Producto::model()->findByPk($inventario->producto_id);
-								$subtotal += $inventario->precio * $uno->cantidad;
+
+                                if($inventario->hasFlashSale()){
+                                    $subtotal += ($inventario->flashSalePrice())*($uno->cantidad); 
+                                }else{
+                                    $subtotal += $inventario->precio * $uno->cantidad;
+                                }
+
                                 $numero_productos += $uno->cantidad;
                                 $peso += $producto->peso * $uno->cantidad;
 
@@ -334,11 +338,21 @@
 								echo ' <tr id="'.$inventario->id.'">
 											<td>'.$im.'</td>
 											<td>
-												<div>'.$producto->nombre.' '.$marca->nombre.'</div>
-												<div>Caracteristicas</div>
-											</td>
-											<td>'.$inventario->precio.' Bs.</td>
-											<td><span id="menos-'.$inventario->id.'" class="glyphicon glyphicon-minus-sign" style="cursor:pointer;" onclick="actualizar(this,'.$inventario->id.');"></span>
+												<div>'.$producto->nombre.' - '.$marca->nombre.'</div>';
+
+                                                if($inventario->hasFlashSale())
+                                                    echo '<div>Aplica Oferta</div>'; 
+
+                                            echo ' 
+											</td>';
+
+                                            if($inventario->hasFlashSale()){
+                                                echo'<td>'.$inventario->flashSalePrice().' Bs.</td>';
+                                            }else{ // <td>'.$inventario->precio.' Bs.</td>
+                                            echo '<td>'.$inventario->precio.' Bs.</td>';
+                                            }
+
+											echo '<td><span id="menos-'.$inventario->id.'" class="glyphicon glyphicon-minus-sign" style="cursor:pointer;" onclick="actualizar(this,'.$inventario->id.');"></span>
 												<span id="cantidad-'.$inventario->id.'">'.$uno->cantidad.'</span>
 												<span id="mas-'.$inventario->id.'" class="glyphicon glyphicon-plus-sign" style="cursor:pointer;" onclick="actualizar(this,'.$inventario->id.');"></span>
 											</td>
