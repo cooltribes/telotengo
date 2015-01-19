c6d88b85f0ba79539a6e43e61293c1ad4825f2d7
Daniel Duque <dduque@upsidecorp.ch>
2015-01-19 15:35:51 -0430

    Detalle de orden, busquedas mejoradas

--- protected/views/producto/_datos_busqueda.php
+++ protected/views/producto/_datos_busqueda.php
@@ -15,11 +15,11 @@
     $marca = Marca::model()->findByPk($data->marca_id);
 		
 	echo "<td><p><strong>".$data->nombre." por <small>".$marca->nombre."</small></strong></p>
-			<p>Descripción del producto: ".$data->descripcion."
+			<p>Descripción del producto: ".substr($data->descripcion,0,75)." ...
 			</p>
 		</td>";								
 								
-    echo ' <td><a href="'.Yii::app()->baseUrl.'/producto/agregarInventario?producto_id='.$data->id.'" class="margin_top btn btn-info btn-sm">Vender este producto</a></td>';
+    echo ' <td><a href="'.Yii::app()->baseUrl.'/producto/inventario/'.$data->id.'" class="margin_top btn btn-info btn-sm">Vender este producto</a></td>';
 
 ?>   
     							
