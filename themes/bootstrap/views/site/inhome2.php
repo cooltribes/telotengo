6d10d21351d425a70fd0e381b21caa2778275415
Wolfredgit config --global username Wolfredgit config --global user.name Wolfred <wmontilla@upsidecorp.ch>
2015-09-24 15:52:41 -0430

    new images on telotengo, add a functio on product's detail.

--- themes/bootstrap/views/site/inhome2.php
+++ themes/bootstrap/views/site/inhome2.php
@@ -60,36 +60,16 @@
            <div class="col-md-3">
                 <div class="row-fluid">
                     <div>
-                        <img width="100%" src="http://placehold.it/294x318">
+                       <img src="<?php echo Yii::app()->theme->baseUrl;?>/images/home/imagenDerachaUp.png" width="100%"/>
                     </div>
                     <div class="margin_top">
-                        <img width="100%" src="http://placehold.it/294x513">
+                        <img src="<?php echo Yii::app()->theme->baseUrl;?>/images/home/imagenDerachaDown.png" width="100%"/>
                     </div>           
                 </div>
            </div>
            
-           <div class="col-md-12 volumeSales margin_top_large">
-               <h1>
-                   OFERTAS DESTACADAS POR VOLUMEN
-               </h1>
-           </div>
-           <div class="col-md-9 volumeSales">
-               <div class="row-fluid">
-                           <?php $this->renderPartial('volume_sales',array('volumeSales'=>NULL)); ?>
-                   
-               </div>
-               
+           <?php #echo $this->renderPartial('ofertaVolumen', array('volumeSales'=>NULL));?>
            
-           </div>
-           <div class="col-md-3 margin_top">
-                <div class="row-fluid">
-                    <div>
-                         <img width="100%" src="http://placehold.it/294x346">
-                    </div>
-                    <div class="margin_top">
-                         <img width="100%" src="http://placehold.it/294x346">
-                    </div>           
-                </div>
-           </div>
+
            
          
