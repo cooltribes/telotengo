67b0f594ffac8ada5b5ca69d50652f07a1a49583
wnmd1987 <wmontilla@upsidecorp.ch>
2016-06-01 15:50:18 -0430

    add graphs on TLT

--- protected/controllers/ControlPanelController.php
+++ protected/controllers/ControlPanelController.php
@@ -44,7 +44,7 @@ class ControlPanelController extends Controller
 		);
 	}
 	
-	public function actionAdmin()
+	/*public function actionAdmin() ////viejo
 	{
 		$empresas = Empresas::model()->findAll(array('order' => 'id'));
 		$mensaje = new Mensajes;
@@ -65,6 +65,161 @@ class ControlPanelController extends Controller
 		}
 			
 		$this->render('admin',array('empresas'=>$empresas,'mensaje'=>$mensaje));
+	}*/
+
+
+	public function actionAdmin()
+	{
+
+		$fechaIni="";
+		$fechaFinal="";
+		if($_GET)
+		{
+			$fechaFinal=$_GET['fechaFinal'];
+			$fechaIni=$_GET['fechaIni'];
+		}
+		else
+		{
+			$fechaFinal=date('Y-m-d');
+			$fechaIni=date('Y-m-d', strtotime('-1 month'));
+		}
+
+		/////////////////////////////////////////Contador///////////////////////////////////////////////////////////////////
+		$sql="select sum(ingresos) as ing from tbl_users";
+		$sumatoria=Yii::app()->db->createCommand($sql)->queryRow();
+		$sumatoria=$sumatoria['ing'];
+		//////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+		//////////////////////////////////////////////////////USUARIOS////////////////////////////////////////////////////
+		$sql='select  * from tbl_users where pendiente=0 and status=1 and type not in(0) and create_at between "'.$fechaIni.'" and "'.$fechaFinal.'" order by create_at asc';
+		$todosUsuarios=User::model()->findAllBysql($sql);	
+		$maxNumb=0;
+		$usuarioDate="";
+		$vectorUsuarioComprador=array();
+		$vectorUsuarioCompraVenta=array();
+		$vectorUsuarioVendedor=array();
+		$vectorFecha=array();
+		foreach($todosUsuarios as $each)
+		{
+			$fecha=date('Y-m-d',strtotime($each->create_at));
+			if($fecha!=$usuarioDate)
+			{
+				$usuarioDate=$fecha;
+				array_push($vectorFecha, Funciones::invertirFecha($fecha, true));
+
+				$sql="select * from tbl_users where pendiente=0 and status=1 and type not in(0) and id in (select userid from tbl_authAssignment where itemname='compraVenta') and cast(create_at as DATE)='".$fecha."'";
+				$compraVenta=User::model()->findAllBySql($sql);
+				array_push($vectorUsuarioCompraVenta, count($compraVenta));
+				if($maxNumb<count($compraVenta))
+					$maxNumb=count($compraVenta);
+
+				$sql="select * from tbl_users where pendiente=0 and status=1 and type not in(0) and id in (select userid from tbl_authAssignment where itemname='vendedor') and cast(create_at as DATE)='".$fecha."'";
+				$vendedor=User::model()->findAllBySql($sql);
+				array_push($vectorUsuarioVendedor, count($vendedor));
+				if($maxNumb<count($vendedor))
+					$maxNumb=count($vendedor);
+
+				$sql="select * from tbl_users where pendiente=0 and status=1 and type not in(0) and id in (select userid from tbl_authAssignment where itemname='comprador') and cast(create_at as DATE)='".$fecha."'";
+				$comprador=User::model()->findAllBySql($sql);
+				array_push($vectorUsuarioComprador, count($comprador));
+				if($maxNumb<count($comprador))
+					$maxNumb=count($comprador);
+			}
+		
+		}
+		/////////////////////////////////////////////Empresas////////////////////////////////////////////////////////////////
+		$sql='select * from tbl_empresas_has_tbl_users empre join tbl_users users on empre.users_id=users.id  where (select min(create_at) from tbl_users) and users.pendiente=0 and users.status=1 and users.create_at between "'.$fechaIni.'" and "'.$fechaFinal.'" group by empre.empresas_id order by empre.empresas_id';
+		$todasEmpresas=Yii::app()->db->createCommand($sql)->queryAll();
+		$maxNumbEmpresas=0;
+		$empresasDate="";
+		$vectorFechaEmpresas=array();
+		$vectorEmpresasComprador=array();
+		$vectorEmpresasVendedor=array();
+		$vectorEmpresasCompraVenta=array();
+		foreach($todasEmpresas as $each)
+		{
+			$fecha=date('Y-m-d',strtotime($each['create_at']));
+			if($fecha!=$empresasDate)
+			{
+				$empresasDate=$fecha;
+				array_push($vectorFechaEmpresas, Funciones::invertirFecha($fecha, true));
+
+				$sql="select * from tbl_empresas_has_tbl_users empre join tbl_users users on empre.users_id=users.id  where (select min(create_at) from tbl_users) and empre.users_id in (select userid from tbl_authAssignment where itemname='compraVenta') and users.pendiente=0 and users.status=1 and cast(users.create_at as DATE)='".$fecha."' group by empre.empresas_id order by empre.empresas_id";
+				$compraVenta=Yii::app()->db->createCommand($sql)->queryAll();
+				array_push($vectorEmpresasCompraVenta, count($compraVenta));
+				if($maxNumbEmpresas<count($compraVenta))
+					$maxNumbEmpresas=count($compraVenta);
+
+				$sql="select * from tbl_empresas_has_tbl_users empre join tbl_users users on empre.users_id=users.id  where (select min(create_at) from tbl_users) and empre.users_id in (select userid from tbl_authAssignment where itemname='vendedor') and users.pendiente=0 and users.status=1 and cast(users.create_at as DATE)='".$fecha."' group by empre.empresas_id order by empre.empresas_id";
+				$vendedor=Yii::app()->db->createCommand($sql)->queryAll();
+				array_push($vectorEmpresasVendedor, count($vendedor));
+				if($maxNumbEmpresas<count($vendedor))
+					$maxNumbEmpresas=count($vendedor);
+
+				$sql="select * from tbl_empresas_has_tbl_users empre join tbl_users users on empre.users_id=users.id  where (select min(create_at) from tbl_users) and empre.users_id in (select userid from tbl_authAssignment where itemname='comprador') and users.pendiente=0 and users.status=1 and cast(users.create_at as DATE)='".$fecha."' group by empre.empresas_id order by empre.empresas_id";
+				$comprador=Yii::app()->db->createCommand($sql)->queryAll();
+				array_push($vectorEmpresasComprador, count($comprador));
+				if($maxNumbEmpresas<count($comprador))
+					$maxNumbEmpresas=count($comprador);
+			}
+		}
+		/////////////////////////////Login de usuarios por rol(solo cuenta un solo login por dia)//////////////////////////////////////////////////
+		$sql='select distinct(cast(fecha as DATE)) as dates from tbl_historial_visitas where fecha between "'.$fechaIni.'" and "'.$fechaFinal.'"';
+		$todosLogin=Yii::app()->db->createCommand($sql)->queryAll();
+		$maxNumbLogin=0;
+		$loginDate="";
+		$vectorFechaLogin=array();
+		$vectorLoginComprador=array();
+		$vectorLoginVendedor=array();
+		$vectorLoginCompraVenta=array();
+		foreach($todosLogin as $each)
+		{
+			$fecha=date('Y-m-d',strtotime($each['dates']));
+			if($fecha!=$loginDate)
+			{
+				$loginDate=$fecha;
+				array_push($vectorFechaLogin, Funciones::invertirFecha($fecha, true));
+
+				$sql="select distinct(id_user) from tbl_historial_visitas where cast(fecha as DATE)='".$fecha."' and id_user in (select userid from tbl_authAssignment where itemname='compraVenta')";
+				$compraVenta=Yii::app()->db->createCommand($sql)->queryAll();
+				array_push($vectorLoginCompraVenta, count($compraVenta));
+				if($maxNumbLogin<count($compraVenta))
+					$maxNumbLogin=count($compraVenta);
+
+				$sql="select distinct(id_user) from tbl_historial_visitas where cast(fecha as DATE)='".$fecha."' and id_user in (select userid from tbl_authAssignment where itemname='vendedor')";
+				$vendedor=Yii::app()->db->createCommand($sql)->queryAll();
+				array_push($vectorLoginVendedor, count($vendedor));
+				if($maxNumbLogin<count($vendedor))
+					$maxNumbLogin=count($vendedor);
+
+				$sql="select distinct(id_user) from tbl_historial_visitas where cast(fecha as DATE)='".$fecha."' and id_user in (select userid from tbl_authAssignment where itemname='comprador')";
+				$comprador=Yii::app()->db->createCommand($sql)->queryAll();
+				array_push($vectorLoginComprador, count($comprador));
+				if($maxNumbLogin<count($comprador))
+					$maxNumbLogin=count($comprador);
+			}
+		}
+		
+
+		$this->render('admin', array('vectorUsuarioCompraVenta'=>$vectorUsuarioCompraVenta,
+									 'vectorUsuarioVendedor'=>$vectorUsuarioVendedor,
+									 'vectorUsuarioComprador'=>$vectorUsuarioComprador,
+									 'vectorFecha'=>$vectorFecha,
+									 'maxNumb'=>$maxNumb,			///////hasta aqui usuarios
+									 'vectorEmpresasCompraVenta'=>$vectorEmpresasCompraVenta,
+									 'vectorEmpresasVendedor'=>$vectorEmpresasVendedor,
+									 'vectorEmpresasComprador'=>$vectorEmpresasComprador,
+									 'vectorFechaEmpresas'=>$vectorFechaEmpresas,
+									 'maxNumbEmpresas'=>$maxNumbEmpresas, //hasta aqui empresas
+									 'vectorLoginCompraVenta'=>$vectorLoginCompraVenta,
+									 'vectorLoginVendedor'=>$vectorLoginVendedor,
+									 'vectorLoginComprador'=>$vectorLoginComprador,
+									 'vectorFechaLogin'=>$vectorFechaLogin,
+									 'maxNumbLogin'=>$maxNumbLogin, ///hasta aqui el login por dia
+									 'sumatoria'=>$sumatoria,
+									 'fechaFinal'=>$fechaFinal,
+									 'fechaIni'=>$fechaIni, 
+									 ));
 	}
 
 	// Uncomment the following methods and override them if needed
--- protected/views/controlPanel/admin.php
+++ protected/views/controlPanel/admin.php
@@ -1,167 +1,235 @@
+<style>
+.naranja{background-color: #ff5b0b;}
+.bolder{font-weight:bolder;}
+.arriba{margin-top: -17px;}
+</style>
 <?php
- 
 $this->breadcrumbs=array(
-	'Panel de Control',
+  'Panel de Control',
 );
-
-$usuarios = User::model()->count();
-$productos_activos = Producto::model()->countByAttributes(array('estado'=>1));
-$ordenes = Orden::model()->findAll();
-$sumatoria = 0;
-$ventas = 0;
-	
-	foreach($ordenes as $orden){
-		if($orden->estado != 5){
-			$sumatoria = $sumatoria + $orden->total;
-			$ventas++;
-		}	
-	}
-
-if($sumatoria != 0)
-	$promedio = $sumatoria / $ventas;
-else
-	$promedio = 0;
 ?>
-<div class="container">
-	<h1>Panel de Control</h1>
-  	<div class="row-fluid">
-    	<div class="col-lg-12">
+<h1 class="orderTitle">Panel de control de empresas</h1>
+<hr>
 
-    		<?php if(Yii::app()->user->hasFlash('success')){?>
-			    <div class="alert in alert-block fade alert-success text_align_center">
-			        <?php echo Yii::app()->user->getFlash('success'); ?>
-			    </div>
-			<?php } ?>
-			<?php if(Yii::app()->user->hasFlash('error')){?>
-			    <div class="alert in alert-block fade alert-danger text_align_center">
-			        <?php echo Yii::app()->user->getFlash('error'); ?>
-			    </div>
-			<?php } ?>
-                
-        	<div class="row-fluid">
-        	    
-        	           <div class="col-md-2 text-center">
-        	               <h3 class="no_margin_bottom"><?php echo $usuarios; ?></h3>
-        	               <h5>Usuarios registrados</h5>
-        	           </div>
-        	           <div class="col-md-2 text-center">
-                           <h3 class="no_margin_bottom"><?php echo $ventas ?></h3>
-                           <h5>Ventas Registradas</h5>
-                       </div>
-                       <div class="col-md-3 text-center">
-                           <h3 class="no_margin_bottom"><?php echo Yii::app()->numberFormatter->formatDecimal($sumatoria); ?></h3>
-                           <h5>Total Vendido<br/><small>(Bs)</small></h5>
-                       </div>
-                       <div class="col-md-3 text-center">
-                           <h3 class="no_margin_bottom"><?php echo Yii::app()->numberFormatter->formatDecimal($promedio); ?></h3>
-                           <h5>Promedio de venta<br/><small>(Bs/Venta)</small></h5>
-                       </div>
-                       <div class="col-md-2 text-center">
-                            <h3 class="no_margin_bottom"><?php echo $productos_activos; ?></h3>
-                            <h5>Productos activos</h5>
-                       </div>
-        	                   
-            </div>
-	      
-    	<div class="row">
-        	<div class="col-lg-12 margin_top">
-         		
-					<h4 class="margin_top">Últimas compras</h4>
-					<hr class="margin_top_xsmall_minus"/>
-
-					<table class="table-striped table">
-			    		<tr>
-			    			<td class="no_border bg_white no_padding_bottom"><b>Cliente</b></td>
-			    			<td class="no_border bg_white no_padding_bottom"><b>Estado</b></td>
-			    			<td class="no_border bg_white no_padding_bottom"><b>Fecha</b></td>
-			    			<td class="no_border bg_white no_padding_bottom"><b>Envio</b></td>
-			    			<td class="no_border bg_white no_padding_bottom"><b>Total</b></td>
-			    		</tr>
-			    		<?php $ultimas_compras = Orden::model()->getLast();
-
-			    		foreach($ultimas_compras as $orden){
-			    			echo "<tr>";
-			    			echo "<td><a href='".Yii::app()->baseUrl."/orden/detalle/".$orden->id."'>".$orden->users->profile->first_name." ".$orden->users->profile->last_name."</a></td>";
-			    			echo $orden->getStatus($orden->estado);
-			    			echo "<td>".date('d-m-Y',strtotime($orden->fecha))."</td>";
-			    			echo "<td>".$orden->envio." Bs.</td>";
-			    			echo "<td>".$orden->total." Bs.</td>";
-			    			echo "</tr>";
-			    		}
-			    		?>
-			    	</table>
-			
-
-			
-			    	<h4 class="margin_top">Ultimos usuarios registrados</h4>
-			    	<hr class="margin_top_xsmall_minus"/>
-	
-			    	<table class="table-striped table">
-			    		<tr>
-			    			<td class="no_border bg_white no_padding_bottom"><b>Nombre</b></td>
-			    			<td class="no_border bg_white no_padding_bottom"><b>Email</b></td>
-			    			<td class="no_border bg_white no_padding_bottom"><b>Fecha de registro</b></td>
-			    			<td class="no_border bg_white no_padding_bottom"><b>Estado</b></td>
-			    		</tr>
-			    		<?php $ultimos_usuarios = User::model()->getLast();
-
-			    		foreach($ultimos_usuarios as $users){
-			    			echo "<tr>";
-	echo "<td><a href='".Yii::app()->baseUrl."/user/profile/profile/id/".$users->id."'>".$users->profile->first_name." ".$users->profile->last_name."</a></td>";
-			    			echo "<td>".$users->email."</td>";
-			    			echo "<td>".date('d-m-Y',strtotime($users->create_at))."</td>";
-			    			echo "<td>";
-								switch ($users->status) {
-									case User::STATUS_NOACTIVE:
-										echo 'Inactivo';
-										break;
-									case User::STATUS_ACTIVE:
-										echo 'Activo';
-										break;
-									case User::STATUS_BANNED:
-										echo 'Suspendido';
-										break;
-									default:
-										echo 'Desconocido';
-										break;
-								}
-							echo "</td>";
-			    			echo "</tr>";
-			    		}
-			    		?>
-			    	</table>
-			
-
-			    	<h4 class="margin_top">Ultimos productos añadidos</h4>
-			    	<hr class="margin_top_xsmall_minus"/>
-			    	<table class="table-striped table">
-			    		<tr>
-			    			<td class="no_border bg_white no_padding_bottom"><b>Nombre</b></td>
-			    			<td class="no_border bg_white no_padding_bottom"><b>Marca</b></td>
-			    			<td class="no_border bg_white no_padding_bottom"><b>Destacado</b></td>
-			    			<td class="no_border bg_white no_padding_bottom"><b>Precio</b></td>
-			    			<td class="no_border bg_white no_padding_bottom"><b>Cantidad</b></td>
-			    		</tr>
-			    		<?php $ultimos_productos = Producto::model()->getLast();
-
-			    		foreach($ultimos_productos as $producto){
-			    			echo "<tr>";
-			    			echo "<td><a href='".$producto->getUrl()."'>".$producto->nombre."</a></td>";
-			    			echo "<td>".$producto->marca->nombre."</td>";
-			    			echo $producto->destacado == 0 ? "<td> No </td>" : "<td> Si </td>" ;
-			    			echo "<td>".$producto->inventarios->precio." Bs.</td>";
-			    			echo "<td>".$producto->inventarios->cantidad."</td>";
-			    			echo "</tr>";
-			    		}
-			    		?>
-
-			    	</table>
-		
-
-		</div>
-      </div>
+<div class="container-fluid">   
+  <div class="row">
+    <div class="clearfix stats">
+             <div class="col-xs-2 stat">
+                 <span class="value"><?php echo count(Empresas::model()->findAll());?></span>
+                 <span class="legend">Total de empresas registradas</span>
+             </div>
+             <div class="col-xs-2 stat">
+                 <span class="value"><?php echo User::model()->countByAttributes(array('status'=>1, 'pendiente'=>0));?></span>
+                 <span class="legend">Total de usuarios activos</span>
+             </div>
+             <div class="col-xs-3 stat">
+                 <span class="value"><?php echo $sumatoria;?></span>
+                 <span class="legend">Total de ingresos a la plataforma</span>
+             </div>
     </div>
+
+  
   </div>
   
+  <div class="row">
+    <div class="col-md-6 col-md-offset-7 arriba">
+​
+      <form action="" class="form-inline">
+        <div class="form-group">        
+          <label class="control-label">Desde:</label>
+              <input type="date" name="fechaIni" value="<?php echo $fechaIni;?>">
+        </div>
+        
+        
+        <div class="form-group">
+          <label class="control-label">Hasta:</label>        
+             <input type="date" name="fechaFinal" value="<?php echo $fechaFinal;?>">
+        </div>
+        <div class="form-group">
+          <button type="submit" class="btn form-control btn-darkgray white naranja">Buscar</button>
+        </div>
+      </form> 
+​
+    </div>
+  </div>
+    
+    
+    
+  <div class="charts-region">
+    <hr>
+    <h3 class="bolder">Empresas registradas</h3>  
+    <div class="row">
+      <div class="col-md-12 ">
+​        <div id="empresas"  style="width: 1000px;"></div>
+        <!--
+        <div class="row margin_top_small">
+              <div class="col-md-2 col-md-offset-3">
+                <label class="control-label">Interval</label>    
+
+              </div>
+              <div class="col-md-6 text-rigth">
+                
+
+                <div class="btn-group" data-toggle="buttons">
+                  <label class="btn btn-default ">
+                    <input type="radio" name="interval" id="option1" value="years" autocomplete="off"> Año
+                  </label>
+                  <label class="btn btn-default ">
+                    <input type="radio" name="interval" id="option2" value="months" autocomplete="off"> Mes
+                  </label> 
+                  <label class="btn btn-default active">
+                    <input type="radio" name="interval" id="option2" value="days" autocomplete="off" checked=""> Dia
+                  </label>          
+                </div>
+
+              </div>
+        </div>-->
+
+      </div>
+           
+    </div>
+    <hr>
+    <h3 class="bolder">Usuarios registrados</h3>    
+    <div class="row">
+      <div class="col-md-12">
+​         <div id="usuarios"  style="width: 1000px;"></div>
+      </div>      
+    </div>
+    <hr>
+    <h3 class="bolder">Login de Usuarios</h3>    
+    <div class="row">
+      <div class="col-md-12">
+​         <div id="login"  style="width: 1000px;"></div>
+      </div>          
+    </div>
+    
+    <h4>Status</h4>    
+    <hr>
+    <div class="row">
+      <div class="col-md-6 chart_status">
+​
+      </div>      
+      <div class="col-md-6">
+​
+      </div>      
+    </div>
+​
+    
+  </div>
 </div>
-<!-- /container -->
+
+
+<script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+
+<script type="text/javascript">
+
+    google.charts.load('current', {'packages':['corechart']});
+
+    google.charts.setOnLoadCallback(usuarios);
+    google.charts.setOnLoadCallback(empresas);
+    google.charts.setOnLoadCallback(login);
+
+  
+
+    function usuarios() 
+    {
+            var vectorUsuarioCompraVenta=<?php echo json_encode($vectorUsuarioCompraVenta);?>;
+            var vectorUsuarioVendedor=<?php echo json_encode($vectorUsuarioVendedor);?>;
+            var vectorUsuarioComprador=<?php echo json_encode($vectorUsuarioComprador);?>;
+            var vectorFecha=<?php echo json_encode($vectorFecha);?>;
+            var maxNumb=<?php echo $maxNumb;?>;
+
+            var data = new google.visualization.DataTable();
+        //data.addColumn('number', 'Usuarios');
+        data.addColumn('string', 'Fecha');
+        data.addColumn('number', 'Compra/Venta');
+        data.addColumn('number', 'Vendedor');
+        data.addColumn('number', 'Comprador');
+
+        for(i = 0; i < vectorFecha.length; i++)
+          data.addRow([vectorFecha[i],vectorUsuarioCompraVenta[i],vectorUsuarioVendedor[i],vectorUsuarioComprador[i]]);
+
+        if(maxNumb<4) // 4 es el numero minimo para que la grafica se vea bien
+          maxNumb=4;
+
+        var options = {
+         // title: 'Usuarios',
+          hAxis: {title: 'Fecha',  titleTextStyle: {color: '#333'}},
+          vAxis: {title: 'Usuarios', minValue: 0, maxValue:maxNumb, format:'0'},
+          legend: {position: 'top', alignment: 'center'},
+
+        };
+
+        var chart = new google.visualization.AreaChart(document.getElementById('usuarios'));
+        chart.draw(data, options);
+      }
+
+      function empresas() 
+      {
+            var vectorEmpresasCompraVenta=<?php echo json_encode($vectorEmpresasCompraVenta);?>;
+            var vectorEmpresasVendedor=<?php echo json_encode($vectorEmpresasVendedor);?>;
+            var vectorEmpresasComprador=<?php echo json_encode($vectorEmpresasComprador);?>;
+            var vectorFechaEmpresas=<?php echo json_encode($vectorFechaEmpresas);?>;
+            var maxNumbEmpresas=<?php echo $maxNumbEmpresas;?>;
+
+            var data = new google.visualization.DataTable();
+        //data.addColumn('number', 'Usuarios');
+        data.addColumn('string', 'Fecha');
+        data.addColumn('number', 'Compra/Venta');
+        data.addColumn('number', 'Vendedor');
+        data.addColumn('number', 'Comprador');
+
+        for(i = 0; i < vectorFechaEmpresas.length; i++)
+          data.addRow([vectorFechaEmpresas[i],vectorEmpresasCompraVenta[i],vectorEmpresasVendedor[i],vectorEmpresasComprador[i]]);
+
+        if(maxNumbEmpresas<4) // 4 es el numero minimo para que la grafica se vea bien
+          maxNumbEmpresas=4;
+
+        var options = {
+         // title: 'Usuarios',
+          hAxis: {title: 'Fecha',  titleTextStyle: {color: '#333'}},
+          vAxis: {title: 'Empresas', minValue: 0, maxValue:maxNumbEmpresas, format:'0'},
+          legend: {position: 'top', alignment: 'center'},
+
+        };
+
+        var chart = new google.visualization.AreaChart(document.getElementById('empresas'));
+        chart.draw(data, options);
+      }
+
+      function login() 
+      {
+            var vectorLoginCompraVenta=<?php echo json_encode($vectorLoginCompraVenta);?>;
+            var vectorLoginVendedor=<?php echo json_encode($vectorLoginVendedor);?>;
+            var vectorLoginComprador=<?php echo json_encode($vectorLoginComprador);?>;
+            var vectorFechaLogin=<?php echo json_encode($vectorFechaLogin);?>;
+            var maxNumbLogin=<?php echo $maxNumbLogin;?>;
+
+            var data = new google.visualization.DataTable();
+        //data.addColumn('number', 'Usuarios');
+        data.addColumn('string', 'Fecha');
+        data.addColumn('number', 'Compra/Venta');
+        data.addColumn('number', 'Vendedor');
+        data.addColumn('number', 'Comprador');
+
+        for(i = 0; i < vectorFechaLogin.length; i++)
+          data.addRow([vectorFechaLogin[i],vectorLoginCompraVenta[i],vectorLoginVendedor[i],vectorLoginComprador[i]]);
+
+        if(maxNumbLogin<4) // 4 es el numero minimo para que la grafica se vea bien
+          maxNumbLogin=4;
+
+        var options = {
+         // title: 'Usuarios',
+          hAxis: {title: 'Fecha',  titleTextStyle: {color: '#333'}},
+          vAxis: {title: 'Login', minValue: 0, maxValue:maxNumbLogin, format:'0'},
+          legend: {position: 'top', alignment: 'center'},
+
+        };
+
+        var chart = new google.visualization.AreaChart(document.getElementById('login'));
+        chart.draw(data, options);
+      }
+
+
+</script>
+
