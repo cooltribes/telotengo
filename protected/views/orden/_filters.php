c5c9760412919d8aa731f1a869ee56c1440f09ae
wnmd1987 <wmontilla@upsidecorp.ch>
2016-07-04 11:55:39 -0430

    remove css

--- protected/views/orden/_filters.php
+++ protected/views/orden/_filters.php
@@ -121,7 +121,7 @@
        <!-- <a href="#" id="filter-save2" class="btn" title="Guardar filtro actual">Guardar Filtro</a> -->
     </div>
     <div class="span1 pull-right"> <!-- Quitar la clase mientras cuando se descomente los filtros de arriba-->
-        <a href="#" id="filter-search" class="btn negro mientras" title="Buscar con el filtro actual">Buscar</a>  
+        <a href="#" id="filter-search" class="btn negro" title="Buscar con el filtro actual">Buscar</a>  
     </div>
     
     
