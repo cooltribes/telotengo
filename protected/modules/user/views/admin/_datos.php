246d497f62ddd69fe46c316864fc499bc69f0ec0
wnmd1987 <wmontilla@upsidecorp.ch>
2016-08-17 16:57:47 -0400

    front end details

--- protected/modules/user/views/admin/_datos.php
+++ protected/modules/user/views/admin/_datos.php
@@ -3,7 +3,7 @@
     <td><?php echo date('d/m/Y',strtotime($data->create_at)); ?></td>
     <td><?php echo $data->profile->first_name." ".$data->profile->last_name; ?></td>
     <td><?php echo $data->email; ?></td>
-    <td><?php echo $data->empresa->razon_social?$data->empresa->razon_social:" N/D "; ?></td>
+    <td><?php echo $data->invited_by->empresa->razon_social?$data->invited_by->empresa->razon_social:" N/D "; ?></td>
 
     <td><?php echo $data->invited_by->profile->first_name." ".$data->invited_by->profile->last_name; ?></td>
     <td><?php echo $data->invited_by->email; ?></td>
